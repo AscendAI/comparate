7e69e8f4a0371e61ba58ab47371bcf8f07cf0c25
arionrefat <arionrefat@gmail.com>
2024-08-22 14:27:02 +0600

    updated footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,18 @@
+import Link from "next/link";
+
 export const Footer = () => {
   return (
     <footer>
       <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          Developed by Gaido
+          <small className="text-sm font-medium leading-none">
+            Developed by
+          </small>
+          <Link href="https://gaido.nl" target="_blank">
+            <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold underline">
+              Gaido
+            </code>
+          </Link>
         </div>
       </div>
     </footer>
